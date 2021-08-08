ac276eb7babd04cad47c2052e71e5b9073cc415a
EUNKYUNG JUNG <thxjek@gmail.com>
2021-08-08 14:35:33 +0900

    UPDATE callback함수, 동기&비동기

--- async/callback.js
+++ async/callback.js
@@ -1,23 +1,5 @@
 "use strict";
 
-//hoisting : var , function declaration
-//Synchronous callback (즉각적으로 나오는 )
-function printImmediately(print) {
-    print();
-}
-
-console.log("1");
-setTimeout(() => console.log("2"), 1000);
-
-console.log("3");
-
-//그럼 callback 내가 부르면 다시와줘!
-//callback은 그럼 비동기일때만 쓰일까?
-//2가지의 경우로 나뉜다.
-
-printImmediately(() => console.log("hello"));
-//Asynchronous callback (나중에~ 언제나올지 예측할 수 없는)
-
 function log(message) {
     console.log(message);
     let test = message;
@@ -101,3 +83,60 @@ const simpleMultiply = (a, b) => {
 (function hello() {
     console.log("IIEE");
 })();
+console.clear();
+//hoisting : var , function declaration
+
+console.log("1");
+//setTimeout(() => console.log("2"), 1000); //비동기적 실행방법
+setTimeout(() => console.log("2"), 1000);
+//우리가 setTimeout으로 function () { //
+//    console.log("2");
+//} 이렇게 전달하는 함수는 바로 실행되는 것이 아니라
+//setTimeout이라는 함수안에 하나의 파라미터 인자로
+//우리가 지정한 즉, 만든 함수를 전달해준다.
+//그래서 지금당장 실행 하지는않고, 1초가 지난 다음에,
+//내 함수를 불러줘~ 라고해서
+//callback "나중에 다시 불러줘~ "라고 해서
+//전달하는 함수들을 callback함수라고하는것이다.
+console.log("3");
+
+//callback 내가 부르면 다시와줘!
+////////////////////////////////////////////////////////////////
+
+//다음으로 그럼, callback은 항상 비동기일때만 쓰일까? 아니다.
+//callback또한 2가지의 경우로 나뉜다.
+
+//Synchronous callback ( '동기' 즉각적으로 나오는 )
+
+//callback을 파라미터인자로 받아서 일을 처리하는 함수
+function printImmediately(print) {
+    //print라는 callback을 받아서
+    print(); //callback 을 바로 실행!
+}
+
+//함수실행
+printImmediately(() => console.log("hello"));
+
+//Asynchronous callback ('비동기' 나중에~ 언제나올지 예측할 수 없는)
+function printWithDelay(print, timeout) {
+    setTimeout(print, timeout);
+}
+
+printWithDelay(() => console.log("async callback"), 2000);
+
+//Callback Hell example
+//
+class UserStorage {
+    loginUser(id, password, onSuccess, onError) {
+        setTimeout(() => {
+            if (
+                (id === "ellie" && password === "dream") ||
+                (id === "coder" && password === "academy")
+            ) {
+                onSuccess(id);
+            } else {
+                onError(new Error("not found"));
+            }
+        }, 2000);
+    }
+}
