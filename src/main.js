37b3b76b239a552c3c9e6241e6fe55b6aa737f67
EUNKYUNG JUNG <thxjek@gmail.com>
2021-08-20 13:59:04 +0900

    Add Jsonfile불러오기

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-loadItem();
+//Fetch the items from the JSON file
+function loadItems() {
+    return fetch("data/data.json")
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+}
+
+//main
+loadItems() //itmes를 동적으로 받아와서 , promis가 retrun(값을 성공적으로 전달해주면 ) 이되면,
+    .then((items) => {
+        // displayItems(items);
+        // setEventListeners(items);
+    })
+    .catch(console.log);
