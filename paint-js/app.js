03f3b89af488305fc5c1f8b926044ed4f860c0b5
EUNKYUNG JUNG <thxjek@gmail.com>
2021-08-21 23:50:38 +0900

    Update function_object

--- paint-js/app.js
+++ paint-js/app.js
@@ -5,3 +5,17 @@ function sayHello(nameOfPerson, age) {
 sayHello("nico", 33);
 sayHello("dal", 22);
 sayHello("lynn", 21);
+
+const player = {
+    name: "elin",
+    sayHello: function (otherPersonsName) {
+        console.log("hello!" + otherPersonsName + "nice to meet you!");
+    },
+};
+
+//나만의 console.log 혹은 array.push와 같은 걸 가지게 되었다.
+//function이 object안에 있을때
+console.log(player.name);
+console.log(player.sayHello("enkyung "));
+player.sayHello("lynn ");
+player.sayHello("nico ");
