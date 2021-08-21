ba32b5e04e2cb2f9f854bf69c0593e80a561e35b
EUNKYUNG JUNG <thxjek@gmail.com>
2021-08-21 23:22:23 +0900

    Add js-basic

---
+++ paint-js/app.js
@@ -0,0 +1,7 @@
+function sayHello(nameOfPerson, age) {
+    console.log("Hello my name is " + nameOfPerson + "and I'm" + age);
+}
+
+sayHello("nico", 33);
+sayHello("dal", 22);
+sayHello("lynn", 21);
